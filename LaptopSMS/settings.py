1112e8d5144aba842501020b033893fadf3b8764
pramod <gopinath.pramod@gmail.com>
2025-07-09 07:18:48 +0530

    index changes

--- LaptopSMS/settings.py
+++ LaptopSMS/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [STATIC_DIR]
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'newstatic')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
